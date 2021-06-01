52c77952e926a25f0e9cb933be4e459cd5032154
Pukar Upreti <pukarupreti3@gmail.com>
2021-06-01 15:04:01 +0545

    Finished Product

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
 import "firebase/app";
-
+import Logo from '../logo.png'
 import {auth} from '../firebase';
 import firebase from 'firebase/app';
+import { Button } from '@material-ui/core'
+import './Login.css'
 
 const Login = () => {
-    return (
-        <div id="login-page">
-            <div id="login-card">
-                <h2>Welcome to Clock-Me</h2>
+    const google = () => {
+        auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    }
 
-                <div className="login-button google"
-                    onClick= {() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
-                >
-                    <GoogleOutlined /> Sign In With Google
-                </div>
-                    <br /><br />
-                    <div className="login-button facebook"
-                    onClick= {() => auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())}
-                 >
-                    <FacebookOutlined /> Sign In With Facebook
-                </div>
+    const facebook = () => {
+        auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+    }
+    return (
+
+        <div className="login">
+            <div className="login__container">
+                <img src={Logo} alt="Clock me"/>
+                <div className="login__text">
+                  <h1>Sign in to Clock Me</h1>
+                </div>  
+             <Button onClick={google}> <GoogleOutlined style={{ padding: "8px" }} /> Sign In With Google</Button> <br /> 
+             <Button onClick={facebook}> <FacebookOutlined style={{ padding: "8px" }} /> Sign In With Facebook</Button>
             </div>
         </div>
     )
