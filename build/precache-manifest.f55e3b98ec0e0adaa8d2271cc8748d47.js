5fef8be549d75819495b9a7a0974cc0eee5be36d
Pukar Upreti <pukarupreti3@gmail.com>
2021-06-01 15:16:30 +0545

    Final Build

---
+++ build/precache-manifest.f55e3b98ec0e0adaa8d2271cc8748d47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d69deddd2ca346c2728b5a85e42c45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58905d2e1a17ef645775",
+    "url": "/static/css/main.eb2bd7eb.chunk.css"
+  },
+  {
+    "revision": "0b0fb93d7a9e5a37eb22",
+    "url": "/static/js/2.fdc2abae.chunk.js"
+  },
+  {
+    "revision": "7fbe34c1931203c17ba36332b80f03ff",
+    "url": "/static/js/2.fdc2abae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58905d2e1a17ef645775",
+    "url": "/static/js/main.6d759742.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
